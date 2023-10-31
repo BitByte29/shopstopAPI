e8e88048cb9bb48c179433ddea9962b52668bc96
bitbyte29 <4gaminghsp@gmail.com>
2023-10-31 19:38:16 +0530

    change order access

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -13,9 +13,10 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
 //If /order/:id route is defined earlier than any toure like /order/new here the new weill then be considered as /order/:id path with id=new to avoid that we can create it in last or choose different route , Faced a problem when did (/order/:id) before (/order/me)
-router
-  .route("/order/:id")
-  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleOrder);
+// router
+// .route("/order/:id")
+// .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
 router.route("/myorders").get(isAuthenticatedUser, myOrders);
 
